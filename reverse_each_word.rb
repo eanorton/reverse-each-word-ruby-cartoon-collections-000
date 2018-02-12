2c5fc3f7d61a581376d31c6f0931d2d57b31d72e
Edward Norton <edwardnorton@icloud.com>
2018-02-12 22:41:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,3 @@
-#def reverse_each_word(string)
-#  string.reverse.split.reverse.join(" ")
-#end
-
 def reverse_each_word(string)
-  array = []
-  array.push(sentence.split)
-  array.collect do |word|
-    word.reverse
-  end
+  string.reverse.split.reverse.join(" ")
 end
